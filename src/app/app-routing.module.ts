d024988c93c95125919f8b20e9a25fb5000779bb
filipeabessa <filipegbessa@gmail.com>
2022-02-07 03:13:35 -0300

    feat(products): :sparkles: Use routeroutlet to enable SPA behavior

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
+import { ProductsListComponent } from './products/products-list/products-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'products', component: ProductsListComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
