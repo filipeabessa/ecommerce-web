31d39dd30065d4ceecdf1d832f231f5e7315532c
filipeabessa <filipegbessa@gmail.com>
2022-02-06 22:24:10 -0300

    feat(products): :sparkles: Create basic product card component

--- src/app/product/product-card/product-card.component.ts
+++ src/app/product/product-card/product-card.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent implements OnInit {
+  product = {
+    name: 'Bola',
+    price: 'R$ 10,00',
+    content: 'Bola de futebol',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
