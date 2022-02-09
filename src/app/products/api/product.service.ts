026bd0cce6be0467d100638fee1351bc025ffa0b
filipeabessa <filipegbessa@gmail.com>
2022-02-09 01:26:11 -0300

    fix(products): :lipstick: Fix the request functions and remove some irrelevant css

--- src/app/products/api/product.service.ts
+++ src/app/products/api/product.service.ts
@@ -22,24 +22,10 @@ export class ProductService {
         price,
         name,
       },
-      headers: {
-        'Content-Type': 'application/json',
-      },
     });
   }
 
   retrieveProduct(id: number): Observable<Product> {
-    return this.httpClient.get<Product>(`${this.apiUrl}/products/${id}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Credentials': 'true',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods':
-          'GET, POST, PATCH, DELETE, PUT, OPTIONS',
-        'Access-Control-Allow-Headers':
-          'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
-      },
-      withCredentials: true,
-    });
+    return this.httpClient.get<Product>(`${this.apiUrl}/products/${id}/`, {});
   }
 }
