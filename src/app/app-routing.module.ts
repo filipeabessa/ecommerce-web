0e8c7e3dd017128fd40bb761eae9a5264cfbda52
filipeabessa <filipegbessa@gmail.com>
2022-02-09 22:52:08 -0300

    feat(shared_module): :sparkles: Create basic /404 component and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { ProductDetailLayoutComponent } from './products/layouts/product-detail-layout/product-detail-layout.component';
-import { ProductsListComponent } from './products/products-list/products-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ProductDetailLayoutComponent } from './products/layouts/product-detail-layout/product-detail-layout.component';
+import { ProductsListComponent } from './products/products-list/products-list.component';
+import { PagNotFoundComponent } from './shared/pag-not-found/pag-not-found.component';
+
 const routes: Routes = [
-  { path: 'products', component: ProductsListComponent },
-  { path: 'product/:id', component: ProductDetailLayoutComponent },
+  {
+    path: 'products',
+    component: ProductsListComponent,
+  },
+  { path: 'products/:id', component: ProductDetailLayoutComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '**', component: PagNotFoundComponent },
 ];
 
 @NgModule({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { PagNotFoundComponent } from './pag-not-found/pag-not-found.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, PagNotFoundComponent],
   imports: [CommonModule, MatToolbarModule, MatButtonModule],
-  exports: [NavbarComponent],
+  exports: [NavbarComponent, PagNotFoundComponent],
 })
 export class SharedModule {}
