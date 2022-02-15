6bfdd360a2e06208eb0e45daf420fd3f0ea7f6ab
filipeabessa <filipegbessa@gmail.com>
2022-02-14 21:56:55 -0300

    feat(shared_module): :bricks: Create MaterialModule and export it in Shared Module

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule,
+    MatSnackBarModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule,
+    MatSnackBarModule,
+  ],
+  providers: [],
+})
+export class MaterialModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 import { PageNotFoundContainerComponent } from './containers/page-not-found-container/page-not-found-container.component';
+import { MaterialModule } from './material.module';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { HttpClientModule } from '@angular/common/http';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [NavbarComponent, PageNotFoundContainerComponent],
-  imports: [CommonModule, MatToolbarModule, MatButtonModule],
-  exports: [NavbarComponent, PageNotFoundContainerComponent],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    StoreModule,
+    HttpClientModule,
+  ],
+  exports: [
+    NavbarComponent,
+    PageNotFoundContainerComponent,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    StoreModule,
+    HttpClientModule,
+  ],
 })
 export class SharedModule {}
